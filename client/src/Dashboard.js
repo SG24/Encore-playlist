b00a0e70f3e7d927930b0f83f5014fcedbdf9a07
shashank <shashankbhatotia@gmail.com>
2019-07-04 18:29:10 +0530

    Adds Routes to Dashboard

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+import Profile from "./Profile";
 
 class Dashboard extends React.Component {
 	render() {
-		return <h1>Voting board</h1>;
+		return (
+			<>
+				<h1>Dashboard</h1>
+				<Link to="/profile">Profile</Link>
+				<BrowserRouter>
+					<Route path="/profile" component={Profile} />
+				</BrowserRouter>
+			</>
+		);
 	}
 }
 
