2366937d175ee447a94a9ca56929666ce08ef33f
shashank <shashankbhatotia@gmail.com>
2019-07-04 17:37:21 +0530

    Adds Button for external GitHub oAuth

--- client/src/App.js
+++ client/src/App.js
@@ -3,29 +3,12 @@ import { BrowserRouter, Redirect, Switch } from "react-router-dom";
 import "./App.scss";
 
 class Login extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			redirect: false
-		};
-	}
-
-	onClickHandler = () => {
-		this.setState({ redirect: true });
-	};
-
 	render() {
-		let { redirect } = this.state;
-		if (redirect) {
-			return (
-				<BrowserRouter>
-					<Redirect exact to="/auth/github" />
-				</BrowserRouter>
-			);
-		}
 		return (
 			<div>
-				<button onClick={this.onClickHandler}>Log in using Github!</button>
+				<button>
+					<a href="/auth/github">Log in using Github!</a>
+				</button>
 			</div>
 		);
 	}
