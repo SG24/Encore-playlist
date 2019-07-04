2f8fc1721c4e2a7e66207f6e875df3d20322cb13
shashank <shashankbhatotia@gmail.com>
2019-07-04 15:06:07 +0530

    Adds Github Login Button in App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,40 @@
-import React, { Component } from 'react';
-import './App.scss';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import "./App.scss";
+
+class Login extends React.Component {
+	// onClickHandler = () => {
+	//   fetch("https://github.com/login/oauth/authorize")
+	// 		.then(response => {
+	// 			if (response.status === 200) {
+	// 				console.log("Connected to github");
+	// 				this.props.history.push("/auth/github");
+	// 			}
+	// 		})
+	// 		.catch(error => console.log(error));
+	// };
+
+	render() {
+		return (
+			// <button onClick={this.onClickHandler}>Login Using Github!</button>
+			<BrowserRouter>
+				<button>
+					<Link to="/auth/github">Login using GitHub!</Link>
+				</button>
+			</BrowserRouter>
+		);
+	}
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Encore</h1>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<h1>Encore</h1>
+				<Login />
+			</div>
+		);
+	}
 }
 
 export default App;
