e8d5098351dcb0a938092bcaddd1a6d04a54eb5d
SG24 <shreyanshgupta98@gmail.com>
2019-07-05 11:54:16 +0530

    makes adding songs available for the users

--- server/routes/api/v1/songs.js
+++ server/routes/api/v1/songs.js
@@ -8,7 +8,8 @@ let songsController = require("./controllers/songsController");
 let jwtAuth = require("./../../../utils/jwtAuth");
 
 // handles routes
-router.post("/new", jwtAuth.authenticateJWT, songsController.newSong)
+router.post("/new", jwtAuth.authenticateJWT, songsController.newSong);
+router.get("/list", jwtAuth.authenticateJWT, songsController.returnSongList);
 
 // exports router
 module.exports = router;
